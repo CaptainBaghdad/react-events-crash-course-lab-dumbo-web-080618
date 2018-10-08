0ae951367b4e3890b0425d346f48471a8203a027
Jason Pitts <juniorjr083@gmail.com>
2018-10-08 21:22:00 +0000

    automatically backed up by learn

--- src/ChromeBoisDomain.js
+++ src/ChromeBoisDomain.js
@@ -42,7 +42,7 @@ export default class ChromeBoisDomain extends Component {
             return resize('+');
         }
         
-        else if(event.keyCode == 83){
+         if(event.keyCode == 83){
           return resize('-');
         }
      
