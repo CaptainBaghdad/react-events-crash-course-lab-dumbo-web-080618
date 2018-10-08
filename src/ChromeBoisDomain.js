44314467a59ecb9a5df9bfd4a4b7e4d06a05b953
Jason Pitts <juniorjr083@gmail.com>
2018-10-08 21:22:41 +0000

    automatically backed up by learn

--- src/ChromeBoisDomain.js
+++ src/ChromeBoisDomain.js
@@ -53,7 +53,7 @@ export default class ChromeBoisDomain extends Component {
       <canvas 
         onMouseMove={this.handleMouseMove}
         onClick={this.customEventHandler}
-        pressedKey={this.pressedKey}
+        ={this.pressedKey}
         width='900'
         height='600'
         tabIndex="0">
